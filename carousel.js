32a52d58a9261e8e0c589549016f37a48b1cf819
ygttncy <yigit03yigit@gmail.com>
2024-05-12 20:05:00 +0300

    Yigit/AP-1-Homepage-Header

--- carousel.js
+++ carousel.js
@@ -1,47 +1,10 @@
-const myCarousel = document.getElementById('myCarousel')
+$(".carousel").carousel({
+  interval: 5000,
+});
 
-myCarousel.addEventListener('slide.bs.carousel', event => {
-  // do something...
-})
-
-const carouselItems = myCarousel.querySelectorAll('.carousel-item')
-
-let currentSlide = 0
-
-const updateCarousel = () => {
-  carouselItems.forEach((item, index) => {
-    if (index === currentSlide) {
-      item.classList.add('active')
-    } else {
-      item.classList.remove('active')
-    }
-  })
-}
-
-const nextSlide = () => {
-  if (currentSlide < carouselItems.length - 1) {
-    currentSlide++
-  } else {
-    currentSlide = 0
-  }
-  updateCarousel()
-}
-
-const prevSlide = () => {
-  if (currentSlide > 0) {
-    currentSlide--
-  } else {
-    currentSlide = carouselItems.length - 1
-  }
-  updateCarousel()
-}
-
-const carouselControlPrev = myCarousel.querySelector('.carousel-control-prev')
-const carouselControlNext = myCarousel.querySelector('.carousel-control-next')
-
-carouselControlPrev.addEventListener('click', prevSlide)
-carouselControlNext.addEventListener('click', nextSlide)
-
-setInterval(() => {
-  nextSlide()
-}, 5000)
+$(document).ready(function() {
+  $('.carousel-indicators span').click(function() {
+    var index = $(this).data('index');
+    $('#carouselExampleIndicators').carousel(index);
+  });
+});
--- script.js
+++
@@ -1,5 +0,0 @@
-const sentence = 'Hello, World!'
-const result = sentence.toLowerCase()
-
-const resultElement = document.getElementById('result')
-resultElement.textContent = `Result: ${result}`
