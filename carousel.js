9ee646f31d967122286e1ce0a50d34105785cc1e
ygttncy <yigit03yigit@gmail.com>
2024-05-13 20:16:51 +0300

    Yigit/AP-1-Homepage-Header

--- carousel.js
+++ carousel.js
@@ -2,9 +2,10 @@ $(".carousel").carousel({
   interval: 5000,
 });
 
-$(document).ready(function() {
-  $('.carousel-indicators span').click(function() {
-    var index = $(this).data('index');
-    $('#carouselExampleIndicators').carousel(index);
+$(document).ready(function () {
+  $(".carousel-indicators span").click(function () {
+    var index = $(this).data("index");
+    $("#carouselExampleIndicators").carousel(index);
   });
 });
+
